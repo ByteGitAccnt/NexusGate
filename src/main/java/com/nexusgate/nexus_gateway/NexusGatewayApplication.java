aacc6b8c0708c22bd9687f2033ddce192aea3395
Shahul <game2018785@gmail.com>
2026-08-19 00:29:19 +0530

    init

---
+++ src/main/java/com/nexusgate/nexus_gateway/NexusGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nexusgate.nexus_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusGatewayApplication.class, args);
+	}
+
+}
